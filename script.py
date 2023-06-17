0e1cf294c149308ef5fc121abae79d270249e776
Bianca Artola <bian.artola@hotmail.com>
2023-06-17 18:32:48 -0300

    * translate script to english

--- script.py
+++ script.py
@@ -1,20 +1,20 @@
 import sys
 
-def palabras_intersectadas(text1, text2):
-    # Obtener las palabras de cada cadena
-    palabras1 = set(text1.split())
-    palabras2 = set(text2.split())
+def find_intersecting_words(string1, string2):
+    # Get the words from each string
+    words1 = set(string1.split())
+    words2 = set(string2.split())
 
-    # Encontrar las palabras intersectadas
-    intersectadas = palabras1.intersection(palabras2)
+    # Find the intersecting words
+    intersecting_words = words1.intersection(words2)
 
-    return intersectadas
+    return intersecting_words
 
 # Check if the required number of arguments is provided
 if len(sys.argv) != 3:
-    print("Usage: python script.py text1 text2")
+    print("Usage: python script.py string1 string2")
 else:
-    text1 = sys.argv[1]
-    text2 = sys.argv[2]
-    resultado = palabras_intersectadas(text1, text2)
-    print(resultado)
+    string1 = sys.argv[1]
+    string2 = sys.argv[2]
+    result = find_intersecting_words(string1, string2)
+    print(result)
